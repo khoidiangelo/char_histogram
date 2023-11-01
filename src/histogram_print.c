23a39c68e7be9b2b4ea3c9ab69d8cca066ee1e62
Tim <horochowiktim@pm.me>
2023-11-01 12:06:15 +0100

    stuff

--- src/histogram_print.c
+++ src/histogram_print.c
@@ -5,19 +5,19 @@ void histogram_print(int clength[], int dlength[], int nother) {
     for (int i = 0; i < MAXCHAR; ++i) {
         printf("%4c%c: ", i + 65, i + 97);
         for (int j = 0; j < clength[i]; ++j) {
-            putchar('|');
+            printf("▌");
         }
         printf("\n");
     }
     for (int i = 0; i < MAXDIG; ++i) {
         printf("%5d: ", i);
         for (int j = 0; j < dlength[i]; ++j) {
-            putchar('|');
+            printf("▌");
         }
         printf("\n");
     }
     printf("other: ");
     for (int i = 0; i < nother; ++i) {
-        putchar('|');
+        printf("▌");
     }
 }
